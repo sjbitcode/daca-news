0b029ffa7d2f5782399ae11856210bd77bc1f15c
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2020-10-30 02:38:15 -0400

    allow hosts in settings

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
