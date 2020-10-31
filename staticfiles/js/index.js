7429a6a452c7d39f17070e97318c881c6523acd3
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2020-10-30 22:53:53 -0400

    Update nav on page change, change color of searchbar

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -6,3 +6,38 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     smallNav.classList.toggle('hidden');
 })
+
+const setPage = () => {
+    const url = document.location.pathname;
+
+    // Change main nav
+    document.querySelectorAll('#main-nav a').forEach(item => {
+        if (item.getAttribute('href') == url) {
+            item.classList.add('text-blue-700')
+        }
+        else {
+            // remove class from any other elements
+            item.classList.remove('text-blue-700')
+        }
+
+    });
+
+    // Change mobile nav
+    document.querySelectorAll('#sm-nav a').forEach(item => {
+        if (item.getAttribute('href') == url) {
+            item.classList.add('text-blue-700')
+        }
+        else {
+            // remove class from any other elements
+            item.classList.remove('text-blue-700')
+        }
+    });
+
+
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    setPage()
+})
+
+
