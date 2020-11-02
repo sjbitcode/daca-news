fa6c844506246603029d957d66b5f856a92b1da0
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2020-11-01 21:22:39 -0500

    ajax pagination functionality

--- articles/views.py
+++ articles/views.py
@@ -67,7 +67,7 @@ class SearchView(ListView):
                 Q(author__contains=query)
             ).order_by('-published_at')
             print(f'GOT SOME ARTICLES ----> {len(article_list)}')
-        return article_list[:12]
+        return article_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -1,13 +1,39 @@
+// ------------------------------------------------------------------
+// Init
+// ------------------------------------------------------------------
+
 const hamburger = document.querySelector('#hamburger');
 const smallNav = document.querySelector('#sm-nav');
+const searchInput = document.querySelector('#dacanews-search');
+const searchButton = document.querySelector('#search-icon');
 
+// toggle mobile/larger nav on screen size changes
 hamburger.addEventListener('click', () => {
-    // showing small nav
-    hamburger.classList.toggle('is-active');
-    smallNav.classList.toggle('hidden');
+    hamburger.classList.toggle('is-active')
+    smallNav.classList.toggle('hidden')
 })
 
-const setPage = () => {
+// When Enter is hit on search bar, trigger the onclick function from searchButton
+searchInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        searchButton.click();
+    }
+})
+
+// Setup things when the page has loaded.
+document.addEventListener('DOMContentLoaded', (e) => {
+    setActivePageLink()
+});
+
+
+// ------------------------------------------------------------------
+// Utils / Helper functions
+// ------------------------------------------------------------------
+
+// Apply active styles on the current page nav link 
+const setActivePageLink = () => {
+
     const url = document.location.pathname;
 
     // Change main nav
@@ -35,53 +61,64 @@ const setPage = () => {
     });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    setPage()
-})
 
-///////////////////////////////////////////////////////////////
+// Remove featured and recent articles if they are there
+const clearFeaturedAndRecentArticles = () => {
 
-const searchInput = document.querySelector('#dacanews-search');
-const searchButton = document.querySelector('#search-icon');
-
-searchInput.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        e.preventDefault();
-        searchButton.click();
-    }
-})
-
-const fetchArticles = async () => {
-    const url = document.location.origin + '/search?q=' + searchInput.value
-    const response = await fetch(url)
-    
-    if (!response.ok) {
-        throw new Error(`Error -- ${response.status}`)
-    }
-    const articles = await response.text()
-    console.log(articles)
-
-    // remove featured articles section if it exists
     let featured_articles = document.getElementById('featured-articles')
     if (featured_articles !== null) {
         featured_articles.remove()
     }
 
-    // add search results
+    let recent_articles = document.getElementById('recent-articles')
+    if (recent_articles !== null) {
+        recent_articles.remove()
+    }
+}
+
+
+// Insert html in div.search-results
+const updateSearchResultsDiv = (html) => {
+
     searchResults = document.getElementById('search-results')
     searchResults.innerHTML = "";
-    searchResults.insertAdjacentHTML('afterbegin', articles)
+    searchResults.insertAdjacentHTML('afterbegin', html)
 }
 
+
+// Utility function for making requests
+const makeRequest = async (url) => {
+
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`Cannot request ${url}, got ${response.status}`)
+    }
+    return await response.text()
+}
+
+
+// Make search request
 const searchAction = () => {
+
     const url = document.location.origin + '/search?q=' + searchInput.value
-    console.log(url);
-    fetchArticles().catch(err => {
-        console.log(err.message)
+    makeRequest(url).then(articles => {
+        clearFeaturedAndRecentArticles()
+        updateSearchResultsDiv(articles)
     })
+    .catch(e => alert(e.message))
+
 }
 
 
+// Make AJAX paginated request
+const fetchPaginated = async (el) => {
 
+    const url = el.getAttribute('data-pagination-url')
+    console.log(`Fetching page - ${url}`)
 
-
+    makeRequest(url).then(articles => {
+        clearFeaturedAndRecentArticles()
+        updateSearchResultsDiv(articles)
+    })
+    .catch(e => alert(e.message))
+}
